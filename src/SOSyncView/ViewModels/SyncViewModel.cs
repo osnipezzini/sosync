f21806b16055d2e0047c60745e694e5b23dcf398
Osni Pezzini Junior <osni@sotech.xyz>
2022-08-09 15:37:08 -0300

    Ajustado tela de sincronia

--- src/SOSyncView/ViewModels/SyncViewModel.cs
+++ src/SOSyncView/ViewModels/SyncViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using SOFramework;
 
+using System.Collections.ObjectModel;
+
 namespace SOSyncView.ViewModels;
 
 public class SyncViewModel : SOViewModel
 {
+    public ObservableCollection<Sync> Bombas { get; set; }
+
 }
