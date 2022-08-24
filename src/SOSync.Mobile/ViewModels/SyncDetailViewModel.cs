ec69f31827bfcd13d8cd2154826c1a364c058e02
BrunoSM <bruno.marian.info@gmail.com>
2022-08-24 15:06:08 -0300

    Efetuado ajustes na interface do SOSync

--- src/SOSync.Mobile/ViewModels/SyncDetailViewModel.cs
+++ src/SOSync.Mobile/ViewModels/SyncDetailViewModel.cs
@@ -1,9 +1,30 @@
-﻿namespace SOSync.Mobile.ViewModels
+﻿using SOSync.Common.Utils;
+
+namespace SOSync.Mobile.ViewModels
 {
     public partial class SyncDetailViewModel : SOViewModel, IQueryAttributable
     {
         [ObservableProperty]
         private Sync sync;
+        private Color color;
+
+        public Color Color { get => color; set => SetProperty(ref color, value); }
+        public SyncDetailViewModel()
+        {
+            Title = $"Detalhes sincronia";
+        }
+
+        private void GetColor()
+        {
+            if (sync is null){
+                Color = Color.FromHex("#000000");
+                return;
+            }
+            else if (sync.Status == StatusImages.OK)
+                Color = Color.FromHex("#4bec00");
+            else
+                Color = Color.FromHex("#FF5525");
+        }
 
         [RelayCommand]
         public Task GoBack() => Shell.Current.GoToAsync("..");
@@ -11,6 +32,8 @@
         {
             if (query.ContainsKey(nameof(Sync)) && query[nameof(Sync)] is Sync sync)
                 Sync = sync;
+
+            GetColor();
         }
     }
 }
--- src/SOSync.Mobile/ViewModels/SyncViewModel.cs
+++ src/SOSync.Mobile/ViewModels/SyncViewModel.cs
@@ -7,10 +7,29 @@ public partial class SyncViewModel : SOViewModel
     public ObservableCollection<Sync> Syncs { get; }
     [ObservableProperty]
     private Sync selectedSync;
+
+    public Command RefreshStatusCommand { get; }
     public SyncViewModel()
 	{
-		Syncs = new ObservableCollection<Sync>();        
+		Syncs = new ObservableCollection<Sync>();
+        RefreshStatusCommand = new Command(async () => await ExecuteRefreshStatusCommand());
 	}
+
+    private async Task ExecuteRefreshStatusCommand()
+    {
+        var listSync = Syncs.ToList();
+        await Task.Delay(200);
+
+        if (listSync[1].Status == StatusImages.OK)
+            listSync[1].Status = StatusImages.Delayed;
+        else
+            listSync[1].Status = StatusImages.OK;
+
+        Syncs.Clear();
+        foreach (var syncDict in listSync)
+            Syncs.Add(syncDict);
+    }
+
     [RelayCommand]
     private Task ShowSyncDetail(Sync sync) => Shell.Current.GoToAsync(nameof(SyncDetailPage), new Dictionary<string, object>
         {
