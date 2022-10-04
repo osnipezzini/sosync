b9fea5c9fd94f62e928bc8038ad3bac7f2abd722
BrunoSM <bruno.marian.info@gmail.com>
2022-10-04 19:31:14 -0300

    Adicionando o assembly info para o correto funcionamento do licenciamento

--- src/SOSync.API/Properties/AssemblyInfo.cs
+++ src/SOSync.API/Properties/AssemblyInfo.cs
@@ -1,15 +1,12 @@
 ﻿using SOCore;
+using SOSync.Common;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-
-[assembly: ExportFont("typicons.ttf", Alias = "TypIcon")]
 [assembly: Guid(AppConstants.ServiceId)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCopyright("Copyright © SOTech 2020-2022")]
 [assembly: SOApplication(AppConstants.Identifier,
     AppName = AppConstants.AppName,
     ModuleId = AppConstants.ServiceId,
-    LogName = "mobile")]
+    LogName = "backend")]
